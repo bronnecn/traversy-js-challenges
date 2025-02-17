4f15f0a35c8662f40481ad0827be56f3b83bbea1
Nicolas Bronnec <nicolas.bronnec@moodys.com>
2025-02-17 18:01:43 +0100

    Challenge 01-03

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,23 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        default:
+            result = 'Invalid operator';
+            break;
+    }
+    return result;
+}
 
 module.exports = calculator;
